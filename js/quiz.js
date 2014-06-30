9b814b1d2f6b49598fcca941cdf17b40352af198
Jarad Comingdeer <jarad@jcomingdeer.com>
2014-06-29 23:54:49 -0700

    Add remaining quiz questions from psd

--- js/quiz.js
+++ js/quiz.js
@@ -2,22 +2,46 @@ var DesignerQuiz = function() {
   var test = {};
   var _questions = [{
     title: "Which Font is This?",
-    customClass: "",
+    customClass: "font",
     imgPath: "img/test_font.png",
     options: ["Helvetica","Arial","Gotham","Futura"],
     answer: 0
   },{
     title: "What is the screen resolution of the iPhone 5s?",
-    customClass: "",
+    customClass: "resolution",
     imgPath: "img/test_font.png",
     options: ["960 X 400","1024 x 768","1136 x 640","1280 x 768"],
     answer: 2
   },{
     title: "Which blending mode is this?",
-    customClass: "",
+    customClass: "blending",
     imgPath: "img/test_font.png",
     options: ["Dissolve","Multiply","Color","Difference"],
+    answer: 2
+  },{
+    title: "How many columns is the default bootstrap grid?",
+    customClass: "bootstrap",
+    imgPath: "img/test_font.png",
+    options: ["8","12","16","24"],
+    answer: 2
+  },{
+    title: "Which of the following is not a type foundry?",
+    customClass: "type",
+    imgPath: "img/test_font.png",
+    options: ["Veer","VSCO","HF&J","House Industries"],
     answer: 3
+  },{
+    title: "Someone that can design and develop is often referred to as:",
+    customClass: "animal",
+    imgPath: "img/test_font.png",
+    options: ["Unicorn","Jackelope","Pegasus","Centaur"],
+    answer: 0
+  },{
+    title: "Which of these is not a to-do app?",
+    customClass: "todo",
+    imgPath: "img/test_font.png",
+    options: ["Clear","Wunderlist","Flipboard","Cheddar"],
+    answer: 2
   }];
       
   var _currentQuestion = 0;
@@ -104,7 +128,7 @@ var DesignerQuiz = function() {
       for (var i = 0; i < _questions.length; i++) {
         var question = _questions[i];
         var slide = $('<li class="question '+question.customClass+'"></li>');
-        if (question.customClass) slide.addClass(customClass);
+        if (question.customClass) slide.addClass(question.customClass);
         
         if (i === 0) {
           slide.addClass('show');
